13581f366410b69b8cd77337e2e302db2ca8d53d
unknown <snetta@yandex.ru>
2025-05-19 23:30:22 +0700

    Add farewell function in feature branch

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -3,10 +3,16 @@
     static void Main(string[] args)
     {
         Console.WriteLine(Greet("World"));
+        Console.WriteLine(Farewell("World"));
     }
 
     static string Greet(string name)
     {
         return $"Hello, {name}!";
+    }
+
+    static string Farewell(string name)
+    {
+        return $"Goodbye, {name}!";
     }
 }
