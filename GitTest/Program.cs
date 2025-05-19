b16bbdd7574b6428691faaf84737ec175e3c94d2
unknown <snetta@yandex.ru>
2025-05-19 23:43:18 +0700

    Add greet_person function in main branch

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -3,10 +3,15 @@
     static void Main(string[] args)
     {
         Console.WriteLine(Greet("World"));
+        Console.WriteLine(GreetPerson("Alice"));
     }
 
     static string Greet(string name)
     {
         return $"Hello, {name}!";
+    }
+    static string GreetPerson(string name)
+    {
+        return $"Welcome, {name}!";
     }
 }
